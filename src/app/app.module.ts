2d903a7a408c03315df9e081e5fbd16d47f63d4f
Stef <stef.guler@gmail.com>
2023-02-23 18:56:38 +0100

    added a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDauwUFNNbmPHpdKubC_-PQs_OvgujKpjc",
+    authDomain: "fir-74b92.firebaseapp.com",
+    databaseURL: "https://fir-74b92-default-rtdb.firebaseio.com",
+    projectId: "fir-74b92",
+    storageBucket: "fir-74b92.appspot.com",
+    messagingSenderId: "909550032827",
+    appId: "1:909550032827:web:1ffabdb61985181c4cd69e"
+  }
+};
